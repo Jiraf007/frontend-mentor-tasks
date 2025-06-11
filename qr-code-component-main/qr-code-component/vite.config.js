0900f3fe4ac5d71c85c7114e486e0e33bf33537c
Anastasiia Makhotkina <makhotkina@metasharks.ru>
2025-06-11 22:09:14 +0300

    change url for gh pages

--- qr-code-component-main/qr-code-component/vite.config.js
+++ qr-code-component-main/qr-code-component/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-mentor-tasks/',
+  base: '/frontend-mentor-tasks/qr-code-component',
   plugins: [react()],
 })
